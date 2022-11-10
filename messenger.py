452beb731ef03316c7ae45a776c8ec2a01879327
gigimerry <69761091+gigimerry@users.noreply.github.com>
2022-11-10 11:08:13 -0500

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -4,14 +4,16 @@ import string
 import hashlib
 import random
 import re
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives.asymmetric.ed25519 import \
         Ed25519PublicKey, Ed25519PrivateKey
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 
 class MessengerServer:
@@ -48,8 +50,9 @@ class MessengerClient:
             return {'private_key': priv_key, 'public_key': pub_key}
 
     def generateCertificate(self):
-
-        DH_keys = GENERATE_DH(self)
+        sk, pk = GENERATE_DH()
+        self.sk = sk
+        self.pk = pk
         certificate = {'name': self.name, 'public_key': DH_keys['public_key']}
 
         raise Exception("not implemented!")
@@ -72,10 +75,14 @@ class MessengerClient:
         return
 
     def GENERATE_DH():
-        
-        return
+        parameters = dh.generate_parameters(generator= 2, key_size= 2048)
+        server_pk = parameters.generate_private_key()
+        person_pk = parameters.generate_private_key()
+        shared_key = server_pk.exchange(person_pk.public_key)
+        return server_pk, shared_key
 
     def DH(dh_pair, dh_pub):
+        
         return
 
     def KDF_RK(rk, dh_out):
@@ -86,13 +93,20 @@ class MessengerClient:
         return key
 
     def KDF_CK(ck):
-        return
+        h = hmac(ck, hashes.SHA256)
+        return h
     
     def ENCRYPT(mk, plaintext, assoc_data):
-        return
+        aesgcm = AESGCM(mk)
+        nonce = os.urandom(12)
+        ct = aesgcm.encrypt(nonce= nonce, data= plaintext, associated_data= assoc_data)
+        return ct
 
     def DECRYPT(mk, ciphertext, assoc_data):
-        return
+        aesgcm = AESGCM(mk)
+        nonce = os.urandom(12)
+        pt = aesgcm.decrypt(nonce= nonce, data= ciphertext, associated_data= assoc_data)
+        return pt
 
     def HEADER(dh_pair, pn, n):
         return
